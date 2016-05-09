2d51479c8831c0b3393d81982d5a9acebce06c82
jemak1 <jemak1@student.monash.edu>
2016-05-09 14:18:47 +1000

    Added map, fixed position of map

--- assignment2/code/js/addlocationPage.js
+++ assignment2/code/js/addlocationPage.js
@@ -26,3 +26,13 @@ var geocoder;
       }
     });
   }
+
+
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+ 
